9da389303e2f9487d81d111d615aba53a86ddf6f
Zirdman <zirdman@gmail.com>
2023-08-30 23:28:31 +0300

    added ascend button functionality

--- index.js
+++ index.js
@@ -13,10 +13,17 @@ _____   ___  ___  ___ _____  ______ _   _ _   _ _____ _____ _____ _____ _   _
 
 let currentScore = 0;
 let scoreMultiplier = 0;
+
+let ascendButtonStatus = 0;
+let ascendButtonCost = 1000;
+let ascendButtonMultiplier = 1;
+
 let upgrade1ButtonStatus = 0;
 let upgrade1ButtonCost = 0;
+
 let upgrade2ButtonStatus = 0;
-let upgrade2ButtonCost = 50;
+let upgrade2ButtonCost = 35;
+let upgrade2ButtonMultiplier = 1;
 
 const display = document.getElementsByClassName("display");
 const scorePerSecond = document.getElementById("scorepersecond");
@@ -33,6 +40,15 @@ const upgrade2Button = document.getElementById("upgrade2");
       |__|  /_____/            \/      \/    \/       \/                              \/             /_____/         \/   
 
 */
+ascendButton.onclick = function upgrade2() {
+    if (currentScore >= ascendButtonCost) {
+        currentScore = currentScore - ascendButtonCost
+        ascendButtonCost = ascendButtonCost * 20;
+        ascendButtonMultiplier = ascendButtonMultiplier * 10;
+        ascendButtonStatus++;
+        ascendButton.innerHTML = "Ascend SPS * 10 Cost: " + ascendButtonCost;
+    }
+}
 upgrade1Button.onclick = function upgrade1() {
     if (currentScore >= upgrade1ButtonCost) {
         currentScore = currentScore - upgrade1ButtonCost;
@@ -45,7 +61,7 @@ upgrade1Button.onclick = function upgrade1() {
 upgrade2Button.onclick = function upgrade2() {
     if (currentScore >= upgrade2ButtonCost) {
         currentScore = currentScore - upgrade2ButtonCost;
-        scoreMultiplier = scoreMultiplier * 2;
+        upgrade2ButtonMultiplier = upgrade2ButtonMultiplier * 1.5;
         upgrade2ButtonStatus = 1;
         upgrade2Button.innerHTML = "Increase SPS * 2 (Level MAX/1)";
     }
@@ -61,14 +77,18 @@ _/  |_|__| _____   ____   __| _/   _______  __ ____   _____/  |_  ______
 */ 
 setInterval(upgradeBlockingTimedEvents, 10);
 function upgradeBlockingTimedEvents() {
+    //ASCEND BUTTON
+    upgrade2Button.innerHTML = "Ascend SPS * 10 Cost: " + ascendButtonCost;
     //UPGRADE 1 BUTTON
     if (upgrade1ButtonStatus === 4) {
         upgrade1Button.disabled = true;
         upgrade1Button.innerHTML = "Increase SPS + 1 (Level MAX/4)"
-        upgrade2Button.hidden = false;
     } else {
         upgrade1Button.disabled = false;
         upgrade1Button.innerHTML = "Increase SPS + 1 (Level " + upgrade1ButtonStatus + "/4) Cost:" + upgrade1ButtonCost;
+    }
+    if (upgrade1ButtonStatus >= 2) {
+        upgrade2Button.hidden = false;
     }
 
     //UPGRADE 2 BUTTON
@@ -77,27 +97,33 @@ function upgradeBlockingTimedEvents() {
         upgrade2Button.innerHTML = "Increase SPS * 2 (Level MAX/1)"
     } else {
         upgrade2Button.disabled = false;
-        upgrade2Button.innerHTML = "Increase SPS * 2 (Level 0/1) Cost:50";
+        upgrade2Button.innerHTML = "Increase SPS * 2 (Level 0/1) Cost:35";
     }
-    
 }
 
 setInterval(timedEvents, 1000);
 function timedEvents() {
     // Score Logic
-    currentScore = currentScore + scoreMultiplier;
+    currentScore = currentScore + (scoreMultiplier * upgrade2ButtonMultiplier);
     document.getElementById("displaytext").innerHTML = currentScore;
 
     // SPS Logic
-    scorePerSecond.innerHTML = scoreMultiplier + " SPS"
+    scorePerSecond.innerHTML = (scoreMultiplier.toFixed(2) * upgrade2ButtonMultiplier.toFixed(2) * ascendButtonMultiplier.toFixed(2)) + " SPS"
 
     //localStorage
     localStorage.savedScore = currentScore;
     localStorage.savedScoreMultiplier = scoreMultiplier;
+        //ascend button
+    localStorage.savedAscendButtonStatus = ascendButtonStatus;
+    localStorage.savedAscendButtonCost = ascendButtonCost;
+    localStorage.savedAscendButtonMultiplier = ascendButtonMultiplier;
+        // upgrade 1 button
     localStorage.savedUpgrade1ButtonStatus = upgrade1ButtonStatus;
     localStorage.savedUpgrade1ButtonCost = upgrade1ButtonCost;
+        // upgrade 2 button
     localStorage.savedUpgrade2ButtonStatus = upgrade2ButtonStatus;
     localStorage.savedUpgrade2ButtonCost = upgrade2ButtonCost;
+    localStorage.savedUpgrade2ButtonMultiplier = upgrade2ButtonMultiplier;
 }
 
 /*
@@ -117,49 +143,64 @@ const deleteButton = document.getElementById("delbutton");
 window.addEventListener("DOMContentLoaded", function() {
     if (localStorage.savedScore) {
         currentScore = parseInt(localStorage.savedScore);
-        document.getElementById("displaytext").innerHTML = currentScore;
-    }
-    
+        document.getElementById("displaytext").innerHTML = currentScore; }
     if (localStorage.savedScoreMultiplier) {
         scoreMultiplier = parseInt(localStorage.savedScoreMultiplier);
-        scorePerSecond.innerHTML = scoreMultiplier + " SPS";
-    }
+        scorePerSecond.innerHTML = scoreMultiplier + " SPS"; }
+        
+    if (localStorage.savedAscendButtonStatus) {
+        ascendButtonStatus = parseInt(localStorage.savedUpgrade1ButtonStatus); }
+    if (localStorage.savedAscendButtonCost) {
+        ascendButtonCost = parseInt(localStorage.savedAscendButtonCost); }
+    if (localStorage.savedAscendButtonMultiplier) {
+        ascendButtonMultiplier = parseInt(localStorage.savedAscendButtonMultiplier); }
 
     if (localStorage.savedUpgrade1ButtonStatus) {
-        upgrade1ButtonStatus = parseInt(localStorage.savedUpgrade1ButtonStatus);
-    }
-
+        upgrade1ButtonStatus = parseInt(localStorage.savedUpgrade1ButtonStatus); }
     if (localStorage.savedUpgrade1ButtonCost) {
-        upgrade1ButtonCost = parseInt(localStorage.savedUpgrade1ButtonCost);
-    }
+        upgrade1ButtonCost = parseInt(localStorage.savedUpgrade1ButtonCost); }
 
     if (localStorage.savedUpgrade2ButtonStatus) {
-        upgrade2ButtonStatus = parseInt(localStorage.savedUpgrade2ButtonStatus);
-    }
-
-    if (localStorage.savedUpgrade1ButtonCost) {
-        upgrade2ButtonCost = parseInt(localStorage.savedUpgrade2ButtonCost);
-    }
+        upgrade2ButtonStatus = parseInt(localStorage.savedUpgrade2ButtonStatus); }
+    if (localStorage.savedUpgrade2ButtonCost) {
+        upgrade2ButtonCost = parseInt(localStorage.savedUpgrade2ButtonCost); }
+    if (localStorage.savedUpgrade2ButtonMultiplier) {
+        upgrade2ButtonMultiplier = parseInt(localStorage.savedUpgrade2ButtonMultiplier); }
 });
 
 deleteButton.onclick = function deleteProgress() {
     currentScore = 0;
     scoreMultiplier = 0;
+
+    ascendButtonStatus = 0;
+    ascendButtonCost = 1000;
+    ascendButtonMultiplier = 1;
+
     upgrade1ButtonStatus = 0;
     upgrade1ButtonCost = 0;
+
     upgrade2ButtonStatus = 0;
-    upgrade2ButtonCost = 0;
+    upgrade2ButtonCost = 35;
+    upgrade2ButtonMultiplier = 1;
 
     document.getElementById("displaytext").innerHTML = currentScore;
     scorePerSecond.innerHTML = scoreMultiplier + " SPS";
+    ascendButton.innerHTML = "Ascend SPS * 10 Cost: " + ascendButtonCost;
     upgrade1Button.innerHTML = "Increase SPS + 1 (Level " + upgrade1ButtonStatus + "/4) Cost:" + upgrade1ButtonCost;
     upgrade2Button.hidden = true;
 
     localStorage.removeItem("savedScore");
     localStorage.removeItem("savedScoreMultiplier");
+
+    localStorage.removeItem("savedAscendButtonStatus");
+    localStorage.removeItem("savedAscendButtonCost");
+    localStorage.removeItem("avedAscendButtonMultiplier");
+
     localStorage.removeItem("savedUpgrade1ButtonStatus");
     localStorage.removeItem("savedUpgrade1ButtonCost");
+
     localStorage.removeItem("savedUpgrade2ButtonStatus");
     localStorage.removeItem("savedUpgrade2ButtonCost");
+    localStorage.removeItem("savedUpgrade2ButtonMultiplier");
 }
 
